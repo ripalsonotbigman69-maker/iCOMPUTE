f1b15db8b3732655bb8183c24e4ef9eadac5ca2a
Ma. Mikaela Ericka A. De Leon <rip.alsonotBIGMAN.69@gmail.com>
2026-06-17 19:22:21 +0800

    database changes

--- backend/python_backend/database.py
+++ backend/python_backend/database.py
@@ -3,10 +3,7 @@ from typing import Generator, Any
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session, declarative_base, sessionmaker
 
-DATABASE_URL = os.environ.get(
-    'DATABASE_URL',
-    'postgresql+pg8000://postgres:IX51m5WX6ymD8Rps@db.qpzfwmjprefyebjprlhc.supabase.co:5432/postgres',
-)
+DATABASE_URL = os.environ.get('DATABASE_URL')
 
 engine: Any = create_engine(DATABASE_URL, future=True)
 SessionLocal: Any = sessionmaker(bind=engine, autoflush=False, autocommit=False, future=True)
